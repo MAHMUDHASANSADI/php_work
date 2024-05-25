d3985963a1d3be74fc0c6333458f62139f9c42ed
MAHMUDHASANSADI <mhasan181075@bscse.uiu.ac.bd>
2024-05-25 14:47:27 +0600

    fibonacci

---
+++ loop.php
@@ -0,0 +1,24 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    //echo $i;
+    echo  PHP_EOL;
+    for($j=0; $j<$i; $j++){
+        echo "*";
+    }
+    echo PHP_EOL;
+}
+echo PHP_EOL;
+
+
+$i=0;
+while($i<10){
+    echo $i;
+    echo PHP_EOL;
+    $i++;
+}
+echo PHP_EOL;
+
+
+
+
+
---
+++ loop01.php
@@ -0,0 +1,5 @@
+<?php
+for($i=10, $j=0;$i>=$j; $i--, $j++){
+    echo $i.":".$j;
+    echo PHP_EOL;
+}
